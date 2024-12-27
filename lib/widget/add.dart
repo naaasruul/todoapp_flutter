75561d7ac4113623143debb3a0552763e2bfb236
naaasruul <nasrulhaqhidayat@gmail.com>
2024-12-27 11:55:24 +0800

    list itle

--- lib/widget/add.dart
+++ lib/widget/add.dart
@@ -1,9 +1,47 @@
 import 'package:flutter/material.dart';
+
 class AddPage extends StatelessWidget {
-  const AddPage({super.key});
+
+  var nameEditingController = TextEditingController();
+  var placeEditingController = TextEditingController();
+  var descEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add new item'),
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: [
+          Text('Add new Item'),
+          TextField(
+            controller: nameEditingController,
+            decoration: InputDecoration(hintText: 'Enter item name: '),
+          ),
+          TextField(
+            controller: placeEditingController,
+            decoration: InputDecoration(hintText: 'Enter item place: '),
+          ),
+          TextField(
+            controller: descEditingController,
+            decoration: InputDecoration(hintText: 'Enter item name: '),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                print(nameEditingController.text);
+                print(placeEditingController.text);
+                print(descEditingController.text);
+
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Add new item',
+                style: TextStyle(color: Colors.black),
+              ))
+        ],
+      ),
+    );
   }
 }
--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/widget/add.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _todos = [
+    {
+      'name': 'Navigation',
+      'desc': 'Simple navigation, pass forward, pass backward data',
+      'place': 'Meeting Room',
+    },
+    {
+      'name': 'Lunch Break',
+      'desc': '1h30 lunch break',
+      'place': 'Own place',
+    },
+    {
+      'name': 'ListView',
+      'desc': 'Listview, ListTile, and Card',
+      'place': 'Meeting Room',
+    },
+    {
+      'name': 'Shared prferences',
+      'desc': 'Save and retrive data',
+      'place': 'Meeting Room',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'ToDo App',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        itemBuilder: (context,index){
+          return ListTile(
+            title: Text(_todos[index]['name']!),
+            subtitle: Text(_todos[index]['place']!),
+            trailing: Icon(Icons.chevron_right),
+          );
+        },
+        itemCount: _todos.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return AddPage();
+          }));
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
